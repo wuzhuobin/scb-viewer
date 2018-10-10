f94d16a814767ad48260cb536e3456722d9798d1
unknown <blazeken1993@yahoo.com.hk>
2018-10-10 17:24:49 +0800

    Merged test2

--- src/dicom-viewer/dicom-viewer.js
+++ src/dicom-viewer/dicom-viewer.js
@@ -5,61 +5,90 @@ import * as cornerstoneTools from "cornerstone-tools";
 import * as cornerstoneMath from "cornerstone-math";
 import dicomLoader from "./dicom-loader";
 import exampleImageIdLoader from "./imageIdLoader";
-import imageIdLoader from "./imageIdLoader";
 
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import daikon from "daikon";
 
 class DicomViewer extends React.Component {
-  currentImageIndex = 0;
-
+  constructor(props){
+    super(props);
+    this.state={
+      username: '',
+      imageId: 0,
+    }
+  }
 
   componentWillMount() {
     cornerstoneTools.external.cornerstone = cornerstone;
     cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
     cornerstoneTools.external.Hammer = Hammer;
-    imageIdLoader(cornerstone);
-    var currentImageIndex = 0;
-
-    setTimeout(function(){    console.log("Read done");
-
-      this.updateTheImage(0);
-      const mouseWheelEvents=['mousewheel','DOMMouseScroll'];
-      mouseWheelEvents.forEach(function(eventType){
-        this.addEventListener(eventType,function(e){
-          if(e.wheelDelta<0 || e.detail>0){
-            if (currentImageIndex === 0){
-              this.updateTheImage(1);
-            }
-          } else {
-            if (currentImageIndex === 1){
-              this.updateTheImage(0);
-            }
-          }
-          return false;
-        })
-      })
-    },4000);
+    dicomLoader(cornerstone);
   }
+
   componentDidMount() {
     this.loadImage();
-  }
-  updateTheImage(imageIndex) {
-    this.currentImageIndex = imageIndex;
-    cornerstone.loadImage(dicomLoader.series.images[this.currentImageIndex]).then(function(image) {
-      if (this.dicomImage){
-        cornerstone.displayImage(this.dicomImage, image);
-      }
-      else {
-        console.log("not newed");
-      }
-    });
   }
 
 
   dicomImage = null;
-  currentImageIndex = 0;
+  HardCodeIdArray = ["example://1", "example://2"];
+
+
+  updateTheImage(imageIndex) {
+    console.log("UpdateTheImage");
+    this.imageId = imageIndex;
+    this.loadImage(this.HardCodeIdArray[imageIndex]
+    // .then(function(image) {
+    //   cornerstone.displayImage(element, image);
+    // }
+    );
+  }
+
+  wheelUp(state){
+    if (state.imageId<this.HardCodeIdArray.length-1){
+      // this.loadImage(this.HardCodeIdArray[state.imageId+1]);
+      console.log(state.imageId+"a->"+String(state.imageId+1));
+      return {...state, imageId: state.imageId+1};
+    }
+    else {
+      // this.loadImage(this.HardCodeIdArray[state.imageId]);
+      console.log(state.imageId+"b->"+String(state.imageId));
+      return state;
+    }
+  }
+  wheelDown(state){
+    console.log(state.imageId);
+    if (state.imageId>0){
+      // this.loadImage(this.HardCodeIdArray[state.imageId-1]);
+      console.log(state.imageId+"c->"+String(state.imageId-1));
+      return {...state, imageId: state.imageId-1};
+    }
+    else {
+      // this.loadImage(this.HardCodeIdArray[state.imageId]);
+      console.log(state.imageId+"d->"+String(state.imageId));
+      return state;
+    }
+  }
+  refreshImage(state){
+    this.loadImage(this.HardCodeIdArray[state.imageId]);
+    console.log('Getting' + this.state.imageId + "-th image");
+  }
+
+  wheelEventsHandler =(e)=> {
+    console.log("wheel_move "+ e.wheelDelta + " ");
+    // console.log("wheel_move "+ e.wheelDelta + " " + this.state.imageId);
+    if (e.wheelDelta < 0 || e.detail > 0) {
+      this.setState(this.wheelDown);
+    } else {
+      this.setState(this.wheelUp);
+    }
+    this.refreshImage(this.state);
+    // Prevent page fom scrolling
+    return false;
+  }
+
+
   loadImage = () => {
     const element = this.dicomImage;
     // Listen for changes to the viewport so we can update the text overlays in the corner
@@ -67,15 +96,15 @@ class DicomViewer extends React.Component {
       const viewport = cornerstone.getViewport(e.target);
       document.getElementById(
         "mrbottomleft"
-      ).textContent = `WW/WC: ${Math.round(
-        viewport.voi.windowWidth
-      )}/${Math.round(viewport.voi.windowCenter)}`;
-      document.getElementById(
-        "mrbottomright"
-      ).textContent = `Zoom: ${viewport.scale.toFixed(2)}`;
-    }
-    element.addEventListener("cornerstoneimagerendered", onImageRendered);
-    const config = {
+        ).textContent = `WW/WC: ${Math.round(
+          viewport.voi.windowWidth
+          )}/${Math.round(viewport.voi.windowCenter)}`;
+        document.getElementById(
+          "mrbottomright"
+          ).textContent = `Zoom: ${viewport.scale.toFixed(2)}`;
+      }
+      element.addEventListener("cornerstoneimagerendered", onImageRendered);
+      const config = {
       // invert: true,
       minScale: 0.25,
       maxScale: 20.0,
@@ -87,9 +116,14 @@ class DicomViewer extends React.Component {
       cornerstoneTools.angle.setConfiguration({ shadow: this.checked });
       cornerstone.updateImage(element);
     });
-    const imageId = "example://1";
+
+    const wheelEvents = ['mousewheel', 'DOMMouseScroll'];
+    for (var i=0;i<wheelEvents.length;i++){
+      element.addEventListener(wheelEvents[i],this.wheelEventsHandler);
+    }
+
     cornerstone.enable(element);
-    cornerstone.loadImage(imageId).then(image => {
+    cornerstone.loadImage(this.HardCodeIdArray[this.state.imageId]).then(image => {
       cornerstone.displayImage(element, image);
       cornerstoneTools.mouseInput.enable(element);
       cornerstoneTools.mouseWheelInput.enable(element);
@@ -97,7 +131,7 @@ class DicomViewer extends React.Component {
       cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
       cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
       cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-      cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+      // cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
       cornerstoneTools.probe.enable(element);
       cornerstoneTools.length.enable(element);
       cornerstoneTools.ellipticalRoi.enable(element);
@@ -132,202 +166,202 @@ class DicomViewer extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-3">
-            <ul className="list-group">
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("wwwc", 1);
-                }}
-                className="list-group-item"
-              >
-                WW/WC
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("pan", 3);
-                }}
-                className="list-group-item"
-              >
-                Pan
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("zoom", 5);
-                }}
-                className="list-group-item"
-              >
-                Zoom
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("length", 1);
-                }}
-                className="list-group-item"
-              >
-                Length
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("probe", 1);
-                }}
-                className="list-group-item"
-              >
-                Probe
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("ellipticalRoi", 1);
-                }}
-                className="list-group-item"
-              >
-                Elliptical ROI
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("rectangleRoi", 1);
-                }}
-                className="list-group-item"
-              >
-                Rectangle ROI
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("angle", 1);
-                }}
-                className="list-group-item"
-              >
-                Angle
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("highlight", 1);
-                }}
-                className="list-group-item"
-              >
-                Highlight
-              </Button>
-              <Button variant="contained" color="primary"
-                onClick={() => {
-                  this.enableTool("freehand", 1);
-                }}
-                className="list-group-item"
-              >
-                Freeform ROI
-              </Button>
-            </ul>
-            <div className="checkbox">
-               <Checkbox value="chkshadow" id="chkshadow" label="Apply shadow"/>
-               Apply shadow
-            </div>
-            <br />
-          </div>
-          <div className="col-9">
-            <div
-              style={{
-                width: 512,
-                height: 512,
-                position: "relative",
-                display: "inline-block",
-                color: "white"
-              }}
-              onContextMenu={() => false}
-              className="cornerstone-enabled-image"
-              unselectable="on"
-              onSelectStart={() => false}
-              onMouseDown={() => false}
-            >
-              <div
-                ref={this.dicomImageRef}
-                style={{
-                  width: 512,
-                  height: 512,
-                  top: 0,
-                  left: 0,
-                  position: "absolute"
-                }}
-              />
-              <div
-                id="mrtopleft"
-                style={{ position: "absolute", top: 3, left: 3 }}
-              >
-                Patient Name: Chan Tai Man
-              </div>
-              <div
-                id="mrtopright"
-                style={{ position: "absolute", top: 3, right: 3 }}
-              >
-                Hospital: PWH
-              </div>
-              <div
-                id="mrbottomright"
-                style={{ position: "absolute", bottom: 3, right: 3 }}
-              >
-                Zoom:
-              </div>
-              <div
-                id="mrbottomleft"
-                style={{ position: "absolute", bottom: 3, left: 3 }}
-              >
-                WW/WC:
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <h3>Functionality</h3>
-            <ul>
-              <li>
-                Activation of a tool for the left mouse button
-                <ul>
-                  <li>
-                    WW/WC - Adjust the window width and window center of the
-                    image (activated by default)
-                  </li>
-                  <li>Pan - Pan the image</li>
-                  <li>Zoom - Zoom the image</li>
-                  <li>Length - Length measurement tool</li>
-                  <li>
-                    Probe - Display the image x,y coordinate under cursor as
-                    well as the pixel value (stored pixel and modality)
-                  </li>
-                  <li>
-                    Elliptical ROI - An elliptical ROI that shows mean, stddev
-                    and area
-                  </li>
-                  <li>
-                    Rectangle ROI - A rectangular ROI that shows mean, stddev
-                    and area
-                  </li>
-                  <li>
-                    Highlight - Darkens the image around a rectangular ROI
-                  </li>
-                  <li>Angle - Cobb angle tool</li>
-                </ul>
-              </li>
-              <li>Use the activated tool by dragging the left mouse button</li>
-              <li>Pan by dragging the middle mouse button</li>
-              <li>Zoom by dragging the right mouse button</li>
-              <li>Zoom by rolling the mouse wheel</li>
-              <li>
-                Tool dragging - left click drag on any measurement tool line to
-                move it
-              </li>
-              <li>
-                Tool deletion - left click drag on any measurement tool line and
-                drop it off the image to delete it
-              </li>
-              <li>
-                Tool handles - left click drag on any measurement tool handle
-                (the circle) to change the handles position
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div className="row">
+      <div className="col-3">
+      <ul className="list-group">
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("wwwc", 1);
+      }}
+      className="list-group-item"
+      >
+      WW/WC
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("pan", 3);
+      }}
+      className="list-group-item"
+      >
+      Pan
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("zoom", 5);
+      }}
+      className="list-group-item"
+      >
+      Zoom
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("length", 1);
+      }}
+      className="list-group-item"
+      >
+      Length
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("probe", 1);
+      }}
+      className="list-group-item"
+      >
+      Probe
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("ellipticalRoi", 1);
+      }}
+      className="list-group-item"
+      >
+      Elliptical ROI
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("rectangleRoi", 1);
+      }}
+      className="list-group-item"
+      >
+      Rectangle ROI
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("angle", 1);
+      }}
+      className="list-group-item"
+      >
+      Angle
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("highlight", 1);
+      }}
+      className="list-group-item"
+      >
+      Highlight
+      </Button>
+      <Button variant="contained" color="primary"
+      onClick={() => {
+        this.enableTool("freehand", 1);
+      }}
+      className="list-group-item"
+      >
+      Freeform ROI
+      </Button>
+      </ul>
+      <div className="checkbox">
+      <Checkbox value="chkshadow" id="chkshadow" label="Apply shadow"/>
+      Apply shadow
       </div>
-    );
-  }
+      <br />
+      </div>
+      <div className="col-9">
+      <div
+      style={{
+        width: 512,
+        height: 512,
+        position: "relative",
+        display: "inline-block",
+        color: "white"
+      }}
+      onContextMenu={() => false}
+      className="cornerstone-enabled-image"
+      unselectable="on"
+      onSelectStart={() => false}
+      onMouseDown={() => false}
+      >
+      <div
+      ref={this.dicomImageRef}
+      style={{
+        width: 512,
+        height: 512,
+        top: 0,
+        left: 0,
+        position: "absolute"
+      }}
+      />
+      <div
+      id="mrtopleft"
+      style={{ position: "absolute", top: 3, left: 3 }}
+      >
+      Patient Name: Chan Tai Man
+      </div>
+      <div
+      id="mrtopright"
+      style={{ position: "absolute", top: 3, right: 3 }}
+      >
+      Hospital: PWH
+      </div>
+      <div
+      id="mrbottomright"
+      style={{ position: "absolute", bottom: 3, right: 3 }}
+      >
+      Zoom:
+      </div>
+      <div
+      id="mrbottomleft"
+      style={{ position: "absolute", bottom: 3, left: 3 }}
+      >
+      WW/WC:
+      </div>
+      </div>
+      </div>
+      </div>
+      <div className="row">
+      <div className="col-xs-12">
+      <h3>Functionality</h3>
+      <ul>
+      <li>
+      Activation of a tool for the left mouse button
+      <ul>
+      <li>
+      WW/WC - Adjust the window width and window center of the
+      image (activated by default)
+      </li>
+      <li>Pan - Pan the image</li>
+      <li>Zoom - Zoom the image</li>
+      <li>Length - Length measurement tool</li>
+      <li>
+      Probe - Display the image x,y coordinate under cursor as
+      well as the pixel value (stored pixel and modality)
+      </li>
+      <li>
+      Elliptical ROI - An elliptical ROI that shows mean, stddev
+      and area
+      </li>
+      <li>
+      Rectangle ROI - A rectangular ROI that shows mean, stddev
+      and area
+      </li>
+      <li>
+      Highlight - Darkens the image around a rectangular ROI
+      </li>
+      <li>Angle - Cobb angle tool</li>
+      </ul>
+      </li>
+      <li>Use the activated tool by dragging the left mouse button</li>
+      <li>Pan by dragging the middle mouse button</li>
+      <li>Zoom by dragging the right mouse button</li>
+      <li>Zoom by rolling the mouse wheel</li>
+      <li>
+      Tool dragging - left click drag on any measurement tool line to
+      move it
+      </li>
+      <li>
+      Tool deletion - left click drag on any measurement tool line and
+      drop it off the image to delete it
+      </li>
+      <li>
+      Tool handles - left click drag on any measurement tool handle
+      (the circle) to change the handles position
+      </li>
+      </ul>
+      </div>
+      </div>
+      </div>
+      );
+}
 }
 
 export default DicomViewer;
