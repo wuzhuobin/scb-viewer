43f9d3fbe0f2fe976f48d320fc7815b9aebf2be7
lokkahei1121@gmail.com <lokkahei1121@gmail.com>
2018-10-15 14:47:03 +0800

    [Add] cornerstone function

--- src/dicom-viewer/dicom-viewer.js
+++ src/dicom-viewer/dicom-viewer.js
@@ -11,15 +11,28 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import Tooltip from '@material-ui/core/Tooltip';
-import Brightness6Icon from '@material-ui/icons/Brightness6';
+import Brightness6Icon from '@material-ui/icons/Brightness6Outlined';
 import OpenWithIcon from '@material-ui/icons/OpenWith';
 import SearchIcon from '@material-ui/icons/Search';
-import StraightenIcon from '@material-ui/icons/Straighten';
+import LinearScaleIcon from '@material-ui/icons/LinearScale';
 import AdjustIcon from '@material-ui/icons/Adjust';
 import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye';
 import CropDinIcon from '@material-ui/icons/CropDin';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import EditIcon from '@material-ui/icons/Edit';
+import CropFreeIcon from '@material-ui/icons/CropFree';
+import NavigationIcon from '@material-ui/icons/NavigationOutlined';
+import MoreIcon from '@material-ui/icons/AddCircleOutline';
+import ClearIcon from '@material-ui/icons/DeleteOutlined';
+import InvertIcon from '@material-ui/icons/Tonality';
+import VFlipIcon from '@material-ui/icons/MoreVert';
+import HFlipIcon from '@material-ui/icons/MoreHoriz';
+import RotateRightIcon from '@material-ui/icons/RotateRight';
+import ReplayIcon from '@material-ui/icons/Replay';
+
+
+import Grid from '@material-ui/core/Grid';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 const styles = theme=> ({
     root:{    
@@ -44,70 +57,80 @@ const styles = theme=> ({
     functionBar:{
           width: '100%',
           overflow: 'auto',
-            }
+            },
+
+    label: {
+    // Aligns the content of the button vertically.
+      flexDirection: 'column'
+            },
 })
 
 class DicomViewer extends React.Component {
+  
   componentWillMount() {
     cornerstoneTools.external.cornerstone = cornerstone;
     cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
     cornerstoneTools.external.Hammer = Hammer;
     dicomLoader(cornerstone);
   }
+  
   componentDidMount() {
     this.loadImage();
   }
+
   dicomImage = null;
+
   loadImage = () => {
+    
     const element = this.dicomImage;
     // Listen for changes to the viewport so we can update the text overlays in the corner
+    
     function onImageRendered(e) {
       const viewport = cornerstone.getViewport(e.target);
-      document.getElementById(
-        "mrbottomleft"
-      ).textContent = `WW/WC: ${Math.round(
-        viewport.voi.windowWidth
-      )}/${Math.round(viewport.voi.windowCenter)}`;
-      document.getElementById(
-        "mrbottomright"
-      ).textContent = `Zoom: ${viewport.scale.toFixed(2)}`;
+      document.getElementById("mrbottomleft").textContent = `WW/WC: ${Math.round(viewport.voi.windowWidth)}/${Math.round(viewport.voi.windowCenter)}`;
+      document.getElementById("mrbottomright").textContent = `Zoom: ${viewport.scale.toFixed(2)}`;
     }
+
     element.addEventListener("cornerstoneimagerendered", onImageRendered);
+    
     const config = {
       // invert: true,
       minScale: 0.25,
       maxScale: 20.0,
-      preventZoomOutsideImage: true
+      preventZoomOutsideImage: true,
     };
+
     cornerstoneTools.zoom.setConfiguration(config);
-    document.getElementById("chkshadow").addEventListener("change", () => {
+
+    const imageId = "example://1";
+    cornerstone.enable(element);
+    cornerstone.loadImage(imageId).then(image => {
+    cornerstone.displayImage(element, image);
+    cornerstoneTools.mouseInput.enable(element);
+    cornerstoneTools.mouseWheelInput.enable(element);
+    // // Enable all tools we want to use with this element
+    cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+    cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+    cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+    cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+    cornerstoneTools.probe.enable(element);
+    cornerstoneTools.length.enable(element);
+    cornerstoneTools.ellipticalRoi.enable(element);
+    cornerstoneTools.rectangleRoi.enable(element);
+    cornerstoneTools.angle.enable(element);
+    cornerstoneTools.highlight.enable(element);
+
       cornerstoneTools.length.setConfiguration({ shadow: this.checked });
       cornerstoneTools.angle.setConfiguration({ shadow: this.checked });
       cornerstone.updateImage(element);
     });
-    const imageId = "example://1";
-    cornerstone.enable(element);
-    cornerstone.loadImage(imageId).then(image => {
-      cornerstone.displayImage(element, image);
-      cornerstoneTools.mouseInput.enable(element);
-      cornerstoneTools.mouseWheelInput.enable(element);
-      // // Enable all tools we want to use with this element
-      cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
-      cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
-      cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-      cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
-      cornerstoneTools.probe.enable(element);
-      cornerstoneTools.length.enable(element);
-      cornerstoneTools.ellipticalRoi.enable(element);
-      cornerstoneTools.rectangleRoi.enable(element);
-      cornerstoneTools.angle.enable(element);
-      cornerstoneTools.highlight.enable(element);
-    });
   };
+
   enableTool = (toolName, mouseButtonNumber) => {
     this.disableAllTools();
     cornerstoneTools[toolName].activate(this.dicomImage, mouseButtonNumber);
   };
+
   // helper function used by the tool button handlers to disable the active tool
   // before making a new tool active
   disableAllTools = () => {
@@ -122,9 +145,12 @@ class DicomViewer extends React.Component {
     cornerstoneTools.highlight.deactivate(this.dicomImage, 1);
     cornerstoneTools.freehand.deactivate(this.dicomImage, 1);
   };
+
   dicomImageRef = el => {
     this.dicomImage = el;
   };
+
+
   render() {
     const {classes} = this.props
 
@@ -132,92 +158,155 @@ class DicomViewer extends React.Component {
       <div className={classes.root}>
            <div className={classes.functionBar}>
               <AppBar position="static" className={classes.appBar}>
-                <Toolbar>
+                <ToggleButtonGroup exclusive >
 
-                  <Tooltip title="Window Level"> 
-                    <Button color="inherit" size="medium" onClick={() => { this.enableTool("wwwc", 1); }}>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => { this.enableTool("wwwc", 1); }}>
+                      <NavigationIcon />
+                      Navigate
+                    </Button>
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => { this.enableTool("wwwc", 1); }}>
                       <Brightness6Icon />
+                      Levels
                     </Button>
-                  </Tooltip>
 
-                  <Tooltip title="Pan"> 
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("pan", 3);}}>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("pan", 3);}}>
                       <OpenWithIcon />
+                      Pan
                     </Button>
-                  </Tooltip>
-
-                  <Tooltip title="Zoom">                   
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("zoom", 5);}}>
+              
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("zoom", 5);}}>
                       <SearchIcon />
+                      Zoom
                     </Button>
-                  </Tooltip>
 
-                  <Tooltip title="Length">   
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("length", 1);}}>
-                      <StraightenIcon />
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("length", 1);}}>
+                      <LinearScaleIcon />
+                      Length
                     </Button>
-                  </Tooltip>
 
-                  <Tooltip title="Angle">   
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("angle", 1);}}>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("angle", 1);}}>
                       <ArrowBackIosIcon />
+                      Angle
                     </Button>
-                  </Tooltip>
-
-                  <Tooltip title="Probe">   
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("probe", 1);}}>
+   
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("probe", 1);}}>
                       <AdjustIcon />
+                      Probe
                     </Button>
-                  </Tooltip>
                   
-                  <Tooltip title="Elliptical ROI">  
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("ellipticalRoi", 1);}}>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("ellipticalRoi", 1);}}>
                       <PanoramaFishEyeIcon />
+                      Elliptical
                     </Button>
-                  </Tooltip>
                   
-                  <Tooltip title="Rectangle ROI">  
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("rectangleRoi", 1);}}>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("rectangleRoi", 1);}}>
                       <CropDinIcon />
+                      Rectangle
                     </Button>
-                  </Tooltip>
-
-                  
-                  <Tooltip title="Freeform ROI">  
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("freehand", 1);}}>
+                   
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("freehand", 1);}}>
                       <EditIcon />
+                      Freeform
                     </Button>
-                  </Tooltip>
 
-                  <Tooltip title="Highlight">  
-                    <Button color="inherit" size="medium" onClick={() => {this.enableTool("highlight", 1);}}>
-                      <EditIcon />
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                      onClick={() => {
+                        const element = this.dicomImage;
+                        const viewport = cornerstone.getViewport(element);
+                        viewport.rotation+=90;
+                        cornerstone.setViewport(element, viewport);}}
+                        >
+                      <RotateRightIcon />
+                      Rotatet
                     </Button>
-                  </Tooltip>
 
-                  <div className="checkbox">
-                     <Checkbox value="chkshadow" id="chkshadow" label="Apply shadow"/>
-                     Apply shadow
-                  </div>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                      onClick={() => {
+                        const element = this.dicomImage;
+                        const viewport = cornerstone.getViewport(element);
+                        viewport.vflip = !viewport.vflip;
+                        cornerstone.setViewport(element, viewport);}}
+                        >
+                      <VFlipIcon />
+                      Flip V
+                    </Button>
 
-                </Toolbar>
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                      onClick={() => {
+                        const element = this.dicomImage;
+                        const viewport = cornerstone.getViewport(element);
+                        viewport.hflip = !viewport.hflip;
+                        cornerstone.setViewport(element, viewport);}}
+                        >
+                      <HFlipIcon />
+                      Flip H
+                    </Button>
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                      onClick={() => {
+                        const element = this.dicomImage;
+                        cornerstone.reset(element);}}
+                        >
+                      <ReplayIcon />
+                      Reset
+                    </Button>
+
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                        onClick={() => {
+                          const element = this.dicomImage;
+                          var viewport = cornerstone.getViewport(element);
+                          if (viewport.invert === true) {
+                              viewport.invert = false;
+                          } else {
+                              viewport.invert = true;
+                          }
+                          cornerstone.setViewport(element, viewport);}}
+                          >
+                      <InvertIcon />
+                      Invert
+                    </Button>
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {this.enableTool("highlight", 1);}}>
+                      <CropFreeIcon />
+                      Highlight
+                    </Button>
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" 
+                        onClick={() => {
+                          const element = this.dicomImage;
+                          var toolStateManager = cornerstoneTools.getElementToolStateManager(element);
+                          toolStateManager.clear(element)
+                          cornerstone.updateImage(element);}}
+                        >
+                      <ClearIcon />
+                      Clear
+                    </Button>
+
+                    <Button classes={{label: classes.label}} color="inherit" size="small" onClick={() => {}}>
+                      <MoreIcon />
+                      More
+                    </Button>
+
+                </ToggleButtonGroup>
               </AppBar>
 
               <div
-              style={{
-                flexGrow: 1,    
-                display: 'flex',
-                width: "100%",
-                height: "100%",
-                position: "relative",
-                display: "inline-block",
-                color: "yellow"
-              }}
-              onContextMenu={() => false}
-              className="cornerstone-enabled-image"
-              unselectable="on"
-              onSelectStart={() => false}
-              onMouseDown={() => false}
+                style={{
+                  flexGrow: 1,    
+                  display: 'flex',
+                  width: "100%",
+                  height: "100%",
+                  position: "relative",
+                  display: "inline-block",
+                  color: "yellow"
+                }}
+                onContextMenu={() => false}
+                className="cornerstone-enabled-image"
+                unselectable="on"
+                onSelectStart={() => false}
+                onMouseDown={() => false}
             >
               
               <div
@@ -232,6 +321,7 @@ class DicomViewer extends React.Component {
                   position: "relative",
                 }}
               />
+
               <div id="mrtopleft" style={{ position: "absolute", top: 3, left: 3 }}>
                 Patient Name: Chan Tai Man
               </div>
@@ -247,10 +337,10 @@ class DicomViewer extends React.Component {
               <div id="mrbottomleft" style={{ position: "absolute", bottom: 3, left: 3 }}>
                 WW/WC:
               </div>
+
+
+          </div>
         </div>
-            </div>
-
-
       </div>
     );
   }
