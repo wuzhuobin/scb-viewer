ce5234345c03c071b5f654365df20aa90a30494b
unknown <blazeken1993@yahoo.com.hk>
2018-11-08 11:24:48 +0800

    [Add]hardcode image

--- src/dicom-viewer/MprViewer.js
+++ src/dicom-viewer/MprViewer.js
@@ -3,11 +3,11 @@ import Hammer from "hammerjs";
 import classNames from 'classnames';
 import {withStyles} from '@material-ui/core/styles'
 import {Paper}  from '@material-ui/core'
-import exampleImageIdLoader from "./exampleImageIdLoader";
 import * as cornerstone from "cornerstone-core";
 import * as cornerstoneTools from "cornerstone-tools";
 import * as cornerstoneMath from "cornerstone-math";
 import dcmViewer from "./dcmViewer"
+import * as dcmLoader from "./dcmLoader";
 
 const styles = theme=> ({
     root:{    
@@ -37,43 +37,104 @@ class MprViewer extends React.Component {
       dicomImage: null,
    	};
   }
+  getImagePathList(IP,Port,Path1){//sync request for now
+
+    if (Path1 == 'Axial'){
+      return new Promise(function(resolve,reject){
+        resolve(['http://192.168.1.112:8080/api/getReslice/2/255']);
+      })   
+    }
+    if (Path1 == 'Sagittal'){
+      return new Promise(function(resolve,reject){
+        resolve(['http://192.168.1.112:8080/api/getReslice/1/255']);
+      })   
+    }
+    if (Path1 == 'Coronal'){
+      return new Promise(function(resolve,reject){
+        resolve(['http://192.168.1.112:8080/api/getReslice/0/255']);
+      })   
+    }
+    
+    return new Promise(function(resolve,reject){
+      resolve(['http://192.168.1.108:8080/0002.png']);
+    })
+
+    // if (Path1===null){
+    //   return new Promise(function(resolve,reject){resolve(["http://223.255.146.2:8042/orthanc/instances/fedab2d3-b15265e7-fa7f9b03-55568349-ef5d91ad/file"])});
+    // }
+    // else{
+    //   return new Promise(function(resolve,reject){
+    //     var queryResult =   fetch("http://223.255.146.2:8042/orthanc/series/" + Path1+ "/ordered-slices").then(
+    //       (res)=>{return res.json();}).then((json)=>{ 
+    //         let cacheImagePathArray = [];
+    //         for(let i = 0; i < json.Dicom.length; ++i){
+    //           let path = "http://223.255.146.2:8042/orthanc" + json.Dicom[i]; 
+    //           cacheImagePathArray.push(path);
+    //         }
+    //     // console.log(cacheImagePathArray);
+    //     return cacheImagePathArray;
+    //   });
+    //       resolve(queryResult);
+
+    //     });
+    // }
+
+  }
+
+  readImage(props, state, hardCodeHint){
+      //Get image path Array first
+      const loadingResult = this.getImagePathList(1,1,hardCodeHint)
+      .then((queryList)=>{
+        var cacheimagePathArray = [];
+        var loaderHint = hardCodeHint;
+
+        const cacheimageLoaderHintsArray = [...Array(queryList.length).keys()].map(function(number){
+          return loaderHint+"://" + String(number);
+        });
+
+
+        for (var i=0;i<queryList.length;i++){
+          cacheimagePathArray.push(queryList[i]);
+        // cacheArray.push("assets/Test1/0"+String((i-i%100)/100)+String((i-(i-i%100)-i%10)/10)+String(i%10)+".dcm");
+      }
+
+      // const stateLoader = this.state.loader;
+      // stateLoader.loadSeries(cacheimagePathArray, loaderHint);
+      dcmLoader.GlobalDcmLoadManager.loadSeries(cacheimagePathArray, loaderHint)
+      // this.setState((state) =>{
+      //   return{
+      //   imagePathArray: cacheimagePathArray,
+      //   imageLoaderHintsArray: cacheimageLoaderHintsArray,
+      //   hardCodeNumDcm: cacheimagePathArray.length,
+      //   previousLoaderHint: loaderHint,
+      //   loader: dcmLoader.GlobalDcmLoadManager.loadSeries(cacheimagePathArray, loaderHint),
+      // }});
+
+    });
+  return loadingResult;
+  }
+
+
+
+
+
 
 
   componentDidMount(){
-
-    const imageId = 'example://1';
-
-    const viewer = new dcmViewer(document.getElementById('dicomImageAxial'))
-    viewer.loadImageFromPacs("4e8739a-f032e726-38354e44-9e636c11-648b3306").then(res=>{
-      console.log('done')
-      viewer.displayImage();
-    })
-
-
-
-
+    this.readImage(null,null,this.props.orientation).then(res=>{
+    // const imageId = 'example://1';
+    console.log(dcmLoader.GlobalDcmLoadManager)
     if (this.props.orientation === "Axial")
     {
-      // const viewerA = document.getElementById('dicomImageAxial');
-      // console.log(viewerA);
-      // viewerA = new dcmViewer()
-      // viewerA.loadImageFromPacs("4e8739a-f032e726-38354e44-9e636c11-648b3306").then(res=>viewerA.displayImage())
-
-
       this.setState({
         dicomImage: document.getElementById('dicomImageAxial')},
         ()=>{
-            // const viewer = new dcmViewer(this.state.dicomImage)
-            // viewer.loadImageFromPacs("4e8739a-f032e726-38354e44-9e636c11-648b3306").then(res=>viewer.displayImage())
-
-
-          // var element = this.state.dicomImage
-          // cornerstone.enable(element);
-          // cornerstone.loadImage(imageId).then(function(image) {
-          //   cornerstone.displayImage(element, image);
-          // })
-          // console.log(this.state.dicomImage)
-          // this.state.dicomImage.readImage(null,'')
+          var element = this.state.dicomImage
+          cornerstone.enable(element);
+          cornerstone.loadImage('Axial://0').then(function(image) {
+            cornerstone.displayImage(element, image);
+            cornerstone.resize(element)
+          })
         })
     }
     else if (this.props.orientation === "Sagittal")
@@ -83,7 +144,7 @@ class MprViewer extends React.Component {
         ()=>{
           var element = this.state.dicomImage
           cornerstone.enable(element);
-          cornerstone.loadImage(imageId).then(function(image) {
+          cornerstone.loadImage('Sagittal://0').then(function(image) {
             cornerstone.displayImage(element, image);
           })
         })
@@ -95,13 +156,14 @@ class MprViewer extends React.Component {
         ()=>{
           var element = this.state.dicomImage
           cornerstone.enable(element);
-          cornerstone.loadImage(imageId).then(function(image) {
+          cornerstone.loadImage('Coronal://0').then(function(image) {
             cornerstone.displayImage(element, image);
           })
         })
     }
 
     window.addEventListener('resize', (event)=>{this.handleResize(event, this.state.dicomImage)})
+    })
   }
 
   componentWillReceiveProps(nextProps) {
@@ -143,7 +205,7 @@ class MprViewer extends React.Component {
           <div className={classes.paper}>
             {orientation === "Axial" 
               && <div id="dicomImageAxial" 
-              className={classNames(classes.dcmViewer, {[classes.drawerOpenDicomViewer]: this.props.drawerOpen})}/>}
+              className={classNames(classes.dicomViewer, {[classes.drawerOpenDicomViewer]: this.props.drawerOpen})}/>}
             {orientation === "Sagittal" 
               && <div id="dicomImageSagittal" 
               className={classNames(classes.dicomViewer, {[classes.drawerOpenDicomViewer]: this.props.drawerOpen})}/>}
